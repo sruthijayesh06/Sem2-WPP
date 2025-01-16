9884e748c9a651931f58b05bfb2f372c7c809697
Sruthi Jayesh <sruthijayesh427@gmail.com>
2025-01-16 22:15:37 +0300

    Update SJ8.py

--- semester 2/SJ8.py
+++ semester 2/SJ8.py
@@ -2,20 +2,6 @@
 #meters, or kilometers. Say if the user enters a 1, then the program converts to inches, if they enter a 2, then the program converts to yards,
 #etc. While this can be done with if statements,it is much shorter with lists and it is also easier to add new conversions if you use lists.
 
-'''
-feet=float(input("Enter length(in feet) : "))
-print("\n1.feet into inches","2.feet into yards","3.feet into meters","4.feet into kilometers",sep="\n")
-opt=int(input("\nEnter option no : "))
-if opt==1 :
-    print("\nLength in inches : ",round(feet*12,4))
-elif opt==2 :
-    print("\nLength in yards : ",round(feet/3,4))
-elif opt==3 :
-    print("\nLength in meters : ",round(feet/3.281,4))
-elif opt==4 :
-    print("\nLength in kilometers : ",round(feet/3281,4))
-'''
-
 feet=float(input("Enter length(in feet) : "))
 L=['inches','yards','meters','kilometers']
 conv=[round(feet*12,4),round(feet/3,4),round(feet/3.281,4),round(feet/3281,4)]
